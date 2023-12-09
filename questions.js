7c002fc1837999337bcec1549185560fa0131ad4
今井 比以呂(Hiiro Imai) <300136@iwaki-kk.co.jp>
2023-12-09 23:10:07 +0900

    first commit

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "HTMLは何の略でしょう?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "CSSは何の略でしょう?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Common Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "JavaScriptにおける'NaN'の意味は?",
+        answer: "A. Not a Number",
+        options: [
+            "A. Not a Number",
+            "B. Not a Name",
+            "C. New and Novel",
+            "D. None and Null"
+        ]
+    },
+    {
+        numb: 4,
+        question: "JSで、'==='演算子は何を意味する?",
+        answer: "C. 同一性",
+        options: [
+            "A. 代入",
+            "B. 等価性",
+            "C. 同一性",
+            "D. 不等価性"
+        ]
+    },
+    {
+        numb: 5,
+        question: "HTMLで画像を埋め込むタグはどれ?",
+        answer: "A. imgタグ",
+        options: [
+            "A. imgタグ",
+            "B. paragraphタグ",
+            "C. imageタグ",
+            "D. graphicsタグ"
+        ]
+    }
+];
